6e77d18ca4f569f269abc96ba2b630b240a6a578
MishaVernik <nodus1.5.3@gmail.com>
2019-06-17 22:03:25 +0300

    wewae

--- main.py
+++ main.py
@@ -88,6 +88,7 @@ def get_text_messages(message):
                 with open("logs.txt", "a+") as f:
                     f.write(str(global_bots) + ": ----BOTS \n Time: " + str(now))
                     print("WROTE")
+                    print(times)
                     f.write(str(times))
                 timeBool = False
             if int(now.second) % 55 != 0 and timeBool == False:
